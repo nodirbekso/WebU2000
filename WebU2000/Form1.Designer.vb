661784a51bb04627979c47c7be691ca0e5399b48
NoBKhamdamov <nodirbekso@gmail.com>
2023-02-26 22:32:03 +0500

    Добавьте файлы проекта.

---
+++ WebU2000/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Controls.Add(Me.RichTextBox2)
+        Me.Panel1.Controls.Add(Me.RichTextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(15, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(774, 608)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(707, 626)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 24)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 285)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(768, 136)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.RichTextBox2.Location = New System.Drawing.Point(6, 427)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(768, 162)
+        Me.RichTextBox2.TabIndex = 1
+        Me.RichTextBox2.Text = ""
+        Me.RichTextBox2.WordWrap = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(610, 626)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(91, 24)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 652)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "WebU2000"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WebU2000/Form1.vb
@@ -0,0 +1,131 @@
+﻿Imports System.IO
+Imports CefSharp
+Imports CefSharp.WinForms
+
+Public Class Form1
+    Dim WithEvents browser As ChromiumWebBrowser
+    Dim qator As Integer
+    Dim qatori As Integer
+
+    Dim KerakQatorlar(1000) As String
+    Dim textSource, allText, lines(), KerakLines(), AvariaLines(500) As String
+    Dim Otuda, Dosuda As Integer
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RichTextBox1.Text = ""
+        RichTextBox2.Text = ""
+        qatori = 0
+    End Sub
+
+    Public Sub New()
+
+        ' Этот вызов является обязательным для конструктора.
+        InitializeComponent()
+
+        ' Добавить код инициализации после вызова InitializeComponent().
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim setting As New CefSettings
+        setting.RemoteDebuggingPort = 8088
+        setting.IgnoreCertificateErrors = True
+        setting.JavascriptFlags = True
+
+
+
+
+
+
+        CefSharp.Cef.Initialize(setting)
+        'browser = New ChromiumWebBrowser("http://google.com") With {
+        '.Dock = DockStyle.Fill
+        '}
+
+        browser = New ChromiumWebBrowser("https://172.30.45.231:31943/") With {
+        .Dock = DockStyle.Fill
+        }
+        'browser.SetZoomLevel(-2.0)
+        Panel1.Controls.Add(browser)
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        getSource()
+        'Dim text = My.Computer.FileSystem.ReadAllText("новый 1.txt")
+        textSource = My.Computer.FileSystem.ReadAllText("currentSource.txt")
+        'RichTextBox1.Text = text
+        'textSource = getSource()
+        qator = 1
+        lines = Split(textSource, "<tr")  'IO.File.ReadLines("c:\111.txt").ToArray
+        For i = 0 To lines.Length - 1
+            RichTextBox1.Text = lines(i)
+            If lines(i).Contains("NodeB Name") Or lines(i).Contains("Site Name") Then
+                KerakQatorlar(qator) = lines(i)
+                qator = qator + 1
+                'RichTextBox2.Text = RichTextBox2.Text & vbCrLf & qator
+            End If
+        Next
+        'MsgBox(qator)
+        For q = 1 To qator - 1
+            allText = allText & KerakQatorlar(q)
+        Next
+        KerakLines = Split(allText, "<tr")
+
+        'MsgBox(KerakLines.Length)
+        qatori = 0
+        For w = 0 To KerakLines.Length - 1
+            'If KerakLines(w).Contains("OML Fault") Or KerakLines(w).Contains("NodeB Unavailable") Then
+            If KerakLines(w).Contains("Site Name=") Or KerakLines(w).Contains("NodeB Name") Then
+                qatori = qatori + 1
+                If InStr(KerakLines(w), "Site Name=") Then
+                    Otuda = InStr(KerakLines(w), "Site Name=") + 10
+                    Dosuda = InStr(Otuda, KerakLines(w), ",")
+                ElseIf InStr(KerakLines(w), "NodeB Name=") Then
+                    Otuda = InStr(KerakLines(w), "NodeB Name=") + 11
+                    Dosuda = InStr(Otuda, KerakLines(w), Chr(34))
+                End If
+
+                'qatori = qatori + 1
+                AvariaLines(qatori) = Mid(KerakLines(w), Otuda, Dosuda - Otuda)
+                'If AvariaLines(w).Contains("tabindex") Then
+                'AvariaLines(w) = Mid(AvariaLines(w), 1, InStr(AvariaLines(w), "tabindex") - 1)
+                'End If
+
+                'RichTextBox2.Text = RichTextBox2.Text & vbCrLf & AvariaLines(w)
+                'RichTextBox2.Text = RichTextBox2.Text & vbCrLf & Mid(KerakLines(w), Otuda, Dosuda - Otuda)
+            End If
+        Next
+        For r = 1 To qatori
+            If AvariaLines(r).Contains("tabindex") Then
+                AvariaLines(r) = Mid(AvariaLines(r), 1, InStr(AvariaLines(r), "tabindex") - 3)
+            End If
+            RichTextBox2.Text = RichTextBox2.Text & vbCrLf & AvariaLines(r)
+
+        Next
+
+
+        MsgBox(qatori)
+    End Sub
+
+    Private Async Function getSource() As Task
+        'Dim source As String = Await browser.GetBrowser().MainFrame.GetSourceAsync()
+        Dim source As String = Await browser.GetBrowser.GetSourceAsync()
+        Dim f As String = Application.StartupPath + "/currentSource.txt"
+        Dim wr As StreamWriter = New StreamWriter(f, False, System.Text.Encoding.Default)
+        wr.Write(source)
+        wr.Close()
+        System.Diagnostics.Process.Start(f)
+        RichTextBox1.Text = source
+
+
+
+
+    End Function
+
+End Class
